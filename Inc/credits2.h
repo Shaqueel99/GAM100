2fa7ca64c19186fca61c6246699ae70e91c21ca1
katlong0709 <katlong0709@gmail.com>
2022-11-24 17:13:20 +0800

    Revert "Merge branch 'ben'"
    This reverts commit 030a4ce53d751b30fb34bdbeb1b5fca568e97b69, reversing
    changes made to e0ad15a113849c04b0ecbb413658e53320ee7353.

--- Inc/credits2.h
+++
@@ -1,5 +0,0 @@
-#pragma once	
-
-void Credits2_Init(void);
-void Credits2_Update(void);
-void Credits2_Exit(void);
--- Src/credits.c
+++ Src/credits.c
@@ -1,6 +1,5 @@
 #include "../Inc/game.h"
 #include "mainmenu.h"
-#include "credits2.h"
 #include "cprocessing.h"
 #include <stdlib.h>
 #include "utils.h"
@@ -17,7 +16,7 @@ struct names {
 };
 float rectCenterX, rectCenterY, rectWidth, rectHeight;
 float rectTopLeftX, rectTopLeftY;
-struct names kitkat, shaq, ben, zy, ck, dx, gerald, team, instructors, prez, clau;
+struct names kitkat, shaq, ben, zy, ck, dx, gerald, pres, team, instructors, claude;
 CP_Sound creditssound = NULL;
 static CP_Image return_menu_button;
 void Credits_Init()
@@ -32,28 +31,26 @@ void Credits_Init()
     white = CP_Color_Create(255, 255, 255, 255);
     
     rectCenterX = width / 2.0f;
-    rectCenterY = height * 0.48f;
+    rectCenterY = height * 0.46f;
     rectWidth = width * 0.55f;
-    rectHeight = height * 0.70f;
+    rectHeight = height * 0.67f;
 
     rectTopLeftX = rectCenterX - rectWidth / 2;
     rectTopLeftY = rectCenterY - rectHeight / 2;
 
     team.y = height / 5.3f;
     kitkat.y = height * 0.25f;
-    zy.y = height * 0.29f;
-    shaq.y = height * 0.33f;
-    ck.y = height * 0.37f;
-    ben.y = height * 0.41f;
+    zy.y = height * 0.3f;
+    shaq.y = height * 0.35f;
+    ck.y = height * 0.4f;
+    ben.y = height * 0.45f;
 
     instructors.y = height / 1.9f;
     dx.y = height * 0.57f;
-    gerald.y = height * 0.61f;
+    gerald.y = height * 0.62f;
 
-    prez.y = height / 1.4f;
-    clau.y = height * 0.76f;
-
-
+    pres.y = height / 1.4f;
+    claude.y = height * 0.76f;
 }
 
 void Credits_Update()
@@ -63,10 +60,10 @@ void Credits_Update()
     CP_Settings_Fill(myColor);
 
     CP_Graphics_ClearBackground(background);
-    image_background = CP_Image_Load("Assets/Credits.png");
+    image_background = CP_Image_Load("Assets/Main_Menu.png");
     logo = CP_Image_Load("Assets/DigiPen_Singapore_WEB_RED.png");
     CP_Image_Draw(image_background, (float)width / 2.0f, height / 2.0f, (float)width, (float)height, 255);
-    CP_Image_Draw(logo, (float)width * 0.5f, height * 0.90f, (float)width * 0.6f, (float)height * 0.13f, 255);
+    CP_Image_Draw(logo, (float)width * 0.5f, height * 0.88f, (float)width * 0.6f, (float)height * 0.13f, 255);
 
     CP_Settings_RectMode(CP_POSITION_CENTER);
 
@@ -80,16 +77,16 @@ void Credits_Update()
     CP_Font_DrawText("All content (c) 2021 ", rectTopLeftX * 1.05f, rectTopLeftY);
     CP_Font_DrawText("DigiPen Institute of Technology Singapore,", rectTopLeftX * 1.05f, rectTopLeftY * 1.15f);
     CP_Font_DrawText("all rights reserved.", rectTopLeftX * 1.05f, rectTopLeftY * 1.30f);
-    CP_Font_DrawText("Press D to next", rectTopLeftX * 2.6f, rectTopLeftY * 6.2f);
 
-    CP_Settings_TextSize(45.0f);
+    CP_Settings_TextSize(50.0f);
     CP_Settings_TextAlignment(CP_TEXT_ALIGN_H_CENTER, CP_TEXT_ALIGN_V_MIDDLE);
     CP_Settings_Fill(white);
     CP_Font_DrawText("Team", rectCenterX, team.y * 1.08f);
     CP_Font_DrawText("Instructors", rectCenterX, instructors.y);
-    CP_Font_DrawText("President", rectCenterX, prez.y);
+    CP_Font_DrawText("President", rectCenterX, pres.y);
 
-    CP_Settings_TextSize(30.0f);
+
+    CP_Settings_TextSize(35.0f);
     CP_Font_DrawText("Yeo Kat Long", rectCenterX, kitkat.y);
     CP_Font_DrawText("Chua Zhi Yu", rectCenterX, zy.y);
     CP_Font_DrawText("Mohammed Shaqeel", rectCenterX, shaq.y);
@@ -99,7 +96,7 @@ void Credits_Update()
     CP_Font_DrawText("Cheng Ding Xiang", rectCenterX, dx.y);
     CP_Font_DrawText("Gerald Wong", rectCenterX, gerald.y);
 
-    CP_Font_DrawText("Claude Comair", rectCenterX, clau.y);
+    CP_Font_DrawText("Claude Comair", rectCenterX, claude.y);
     
     if (CP_Input_KeyTriggered(KEY_ESCAPE)) {
         CP_Engine_SetNextGameStateForced(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit);
@@ -112,9 +109,6 @@ void Credits_Update()
     //CP_Image return_menu_button = CP_Image_Load("Assets/game_ui/return_button.png");
     CP_Image_Draw(return_menu_button, width - height * 0.05f, height - height * 0.05f, height * 0.1f, height * 0.1f, 255);
     //CP_Image_Free(&return_menu_button);
-    if (CP_Input_KeyTriggered(KEY_D)) {
-        CP_Engine_SetNextGameStateForced(Credits2_Init, Credits2_Update, Credits2_Exit);
-    }
 }
 
 void Credits_Exit()
